340c84b6e27200cbd8885cfa9ab49c8286214a06
themanyfaceddemon <gastermini357@gmail.com>
2026-04-13 00:57:58 +0300

    cleanup

--- t.py
+++ t.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-from plg_reader._python_reader import PyRead
+from plg_reader._python_reader.text_to_tokens import PyRead
 
 p = PyRead.read_file_to_tokens(
     Path(r"F:\Desktop\plg\plg-reader\plg_reader\_python_reader\text_to_tokens.py")
