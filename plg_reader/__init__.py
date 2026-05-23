1a41eb4fd81693ba5829855ff8b60b03bfb48b58
themanyfaceddemon <gastermini357@gmail.com>
2026-05-23 08:07:10 +0300

    postfix

--- plg_reader/__init__.py
+++ plg_reader/__init__.py
@@ -1,6 +1,8 @@
 from ._python import build_python_file, build_python_files_dir
+from ._utils import BinaryRW
 
 __all__ = [
     "build_python_file",
     "build_python_files_dir",
+    "BinaryRW",
 ]
